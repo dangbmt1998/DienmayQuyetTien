e0ef956b8e4c4e7be0158e897bc6a80bca2f1028
TuanMadMax <thienhabinhan@gmail.com>
2018-12-05 12:07:30 +0700

    Update,create hien thi hinh anh,unit test index

---
+++ WebApplication1/WebApplication1.Tests/Controllers/BangSanPhamTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Collections.Generic;
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class BangSanPhamTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new BangsanphamController();
+            var result = controller.Index() as ViewResult;
+            var db = new CS4PEEntities();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<BangSanPham>));
+            Assert.AreEqual(db.BangSanPhams.Count(), ((List<BangSanPham>)result.Model).Count);
+        }
+    }
+}
--- WebApplication1/WebApplication1/Controllers/BangsanphamController.cs
+++ WebApplication1/WebApplication1/Controllers/BangsanphamController.cs
@@ -22,15 +22,10 @@ namespace WebApplication1.Controllers
         }
 
         // GET: /Bangsanpham/Details/5
-        public ActionResult Details(int id)
+        public FileResult Details(string id)
         {
-            var model = db.BangSanPhams.Find(id);
-            if (model == null)
-            {
-                return HttpNotFound(); ;
-            }
-            
-            return View(model);
+            var path = Server.MapPath("~/App_Data/" + id);
+            return File(path, "images");
         }
 
         // GET: /Bangsanpham/Create
@@ -64,7 +59,8 @@ namespace WebApplication1.Controllers
                     scope.Complete();
                     return RedirectToAction("Index");
                 }
-                return RedirectToAction("Index");
+                else { ModelState.AddModelError("HinhAnh", "Chưa có hình ảnh"); }
+                
                 }
             }
 
@@ -84,19 +80,15 @@ namespace WebApplication1.Controllers
         }
 
         // GET: /Bangsanpham/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            BangSanPham bangsanpham = db.BangSanPhams.Find(id);
-            if (bangsanpham == null)
+            BangSanPham model = db.BangSanPhams.Find(id);
+            if (model == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
-            return View(bangsanpham);
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
         }
 
         // POST: /Bangsanpham/Edit/5
@@ -104,16 +96,28 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include="id,MaSP,TenSP,Loai_id,GiaBan,GiaGoc,GiaGop,SoLuongTon")] BangSanPham bangsanpham)
+        public ActionResult Edit(BangSanPham model)
         {
+            CheckBangSanPham(model);
             if (ModelState.IsValid)
             {
-                db.Entry(bangsanpham).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                using (var scope = new TransactionScope())
+                {
+                    db.Entry(model).State = EntityState.Modified;
+                    db.SaveChanges();
+                    var path = Server.MapPath("~/App_Data");
+                    path = path + "/" + model.id;
+                    if (Request.Files["HinhAnh"] != null &&
+                        Request.Files["HinhAnh"].ContentLength > 0)
+                    {
+                        Request.Files["HinhAnh"].SaveAs(path);
+                    }
+                    scope.Complete(); // approve for transaction
+                    return RedirectToAction("Index");
+                }
             }
-            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", bangsanpham.Loai_id);
-            return View(bangsanpham);
+            ViewBag.Loai_id = new SelectList(db.LoaiSanPhams, "id", "TenLoai", model.Loai_id);
+            return View(model);
         }
 
         // GET: /Bangsanpham/Delete/5
